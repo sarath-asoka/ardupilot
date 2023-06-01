ec7a85287972a9c5079369bcc932333e5f2e5229
sarath-asoka <sarathasoka.official@gmail.com>
2023-06-01 15:08:41 +0900

    New flight mode called SPIRAL

---
+++ ArduCopter/mode_spiral.cpp
@@ -0,0 +1,78 @@
+/*
+Team : Spiral Inc
+Author : Sarath Asoka
+Description : New mode spiral will imitate alt_hold mode.
+*/
+
+#include "Copter.h"
+
+#if MODE_SPIRAL_ENABLED == ENABLED
+/*
+ * Init and run calls for spiral flight mode
+ */
+
+// spiral_init - initialise spiral controller
+bool ModeSpiral::init(bool ignore_checks)
+{
+    return true;
+}
+
+// stabilize_run - runs the main stabilize controller
+// should be called at 100hz or more
+void ModeSpiral::run()
+{
+    // apply simple mode transform to pilot inputs
+    update_simple_mode();
+
+    // convert pilot input to lean angles
+    float target_roll, target_pitch;
+    get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, copter.aparm.angle_max);
+
+    // get pilot's desired yaw rate
+    float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());
+
+    if (!motors->armed()) {
+        // Motors should be Stopped
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::SHUT_DOWN);
+    } else if (copter.ap.throttle_zero){
+        // Attempting to Land
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::GROUND_IDLE);
+    } else {
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+    }
+
+    switch (motors->get_spool_state()) {
+    case AP_Motors::SpoolState::SHUT_DOWN:
+        // Motors Stopped
+        attitude_control->reset_yaw_target_and_rate();
+        attitude_control->reset_rate_controller_I_terms();
+        pilot_desired_throttle = 0.0f;
+        break;
+
+    case AP_Motors::SpoolState::GROUND_IDLE:
+        // Landed
+        attitude_control->reset_yaw_target_and_rate();
+        attitude_control->reset_rate_controller_I_terms_smoothly();
+        pilot_desired_throttle = 0.0f;
+        break;
+
+    case AP_Motors::SpoolState::THROTTLE_UNLIMITED:
+        // clear landing flag above zero throttle
+        if (!motors->limit.throttle_lower) {
+            set_land_complete(false);
+        }
+        break;
+
+    case AP_Motors::SpoolState::SPOOLING_UP:
+    case AP_Motors::SpoolState::SPOOLING_DOWN:
+        // do nothing
+        break;
+    }
+
+    // call attitude controller
+    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
+
+    // output pilot's throttle
+    attitude_control->set_throttle_out(pilot_desired_throttle, true, g.throttle_filt);
+}
+#endif
